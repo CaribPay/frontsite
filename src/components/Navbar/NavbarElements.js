b8b106219d8121ed2c1192a7c4c09be000e7e091
Elektropay <richard.r@everpayinc.com>
2022-01-29 23:56:01 -0500

    Update NavbarElements.js

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -30,7 +30,7 @@ export const NavbarContainer = styled.div`
 `;
 
 export const NavLogo = styled(LinkR)`
-  color: #FFF;
+  color: #101522;
   justify-self: flex-start;
   cursor: pointer;
   font-size: 1.5rem;
