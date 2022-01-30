35f5e4d5946503bf198986decb449ba0f30c9f0a
Elektropay <richard.r@everpayinc.com>
2022-01-29 23:30:32 -0500

    Update ServicesElements.js

--- src/components/Services/ServicesElements.js
+++ src/components/Services/ServicesElements.js
@@ -68,9 +68,11 @@ export const ServicesH1 = styled.h1`
 export const ServicesH2 = styled.h2`
   font-size: 1rem;
   margin-bottom: 10px;
+    color: #101522;
 `;
 
 export const ServicesP = styled.p`
   font-size: 1rem;
   text-align: center;
+  color: #101522;
 `;
